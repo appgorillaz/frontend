737e0cdeaf20b78e3f59da9cdf48917988915b31
toledo <guhh03odelot@gmail.com>
2024-06-03 13:04:11 -0300

    desktop 1v

--- scripts/navBar.mjs
+++ scripts/navBar.mjs
@@ -3,10 +3,11 @@
 //botoes nav bar
 const btn_home = document.getElementById('btn-nav_home');
 const btn_calendario = document.getElementById('btn-nav_calendario');
-
 const btn_perfil = document.getElementById('btn-nav_perfil');
 
-
+const btnDeskHome = document.getElementById('btn-nav-desk_home');
+const btnDeskCalen = document.getElementById('btn-nav-desk_calendario');
+const btnDeskPerfil = document.getElementById('btn-nav-desk_perfil');
 
 
 
@@ -19,3 +20,7 @@ function mudarPagina(botao, pagina) {
 mudarPagina(btn_home, "../feed.html");
 mudarPagina(btn_calendario, "../pages/calendario.html")
 mudarPagina(btn_perfil, "../pages/perfil.html")
+
+mudarPagina(btnDeskHome, "../feed.html");
+mudarPagina(btnDeskCalen, "../pages/calendario.html")
+mudarPagina(btnDeskPerfil, "../pages/perfil.html")
