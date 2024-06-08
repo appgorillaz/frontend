f6de0586f18b24bff5ffb218f1253d5148f152cb
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-06-08 20:37:27 -0300

    feat: check token expiration and add buttons div on post container

--- scripts/interacao.js
+++ scripts/interacao.js
@@ -1,4 +1,21 @@
-if (!localStorage.getItem("token")) {
+const isTokenExpired = (token) => {
+  if (!token) return true;
+
+  const decoded = jwt_decode(token);
+  const currentTime = Date.now() / 1000;
+
+  // Verifica se o token já expirou
+  if (decoded.exp < currentTime) {
+    return true;
+  }
+
+  return false;
+};
+
+if (
+  !localStorage.getItem("token") ||
+  isTokenExpired(localStorage.getItem("token"))
+) {
   window.location.href = "./pages/login.html";
 }
 
@@ -10,10 +27,11 @@ const getPosts = async () => {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     })
-    .then((res) => (posts = res.data))
+    .then((res) => {
+      posts = res.data;
+    })
     .catch((err) => console.log(err));
 
-  //crie um h2 para receber o titulo do post, um p para receber o texto e um p para receber a dataadicione esses elementos ao containerPosts
   const containerPosts = document.querySelector(".container-cards");
   posts.forEach((post) => {
     const div = document.createElement("div");
@@ -32,9 +50,37 @@ const getPosts = async () => {
     pData.textContent = `Publicado em ${dia}/${mes}/${ano}`;
     pData.classList.add("data_post");
 
+    //criando div para interações de like e comentario
+    const divInteractions = document.createElement("div");
+    divInteractions.classList.add("container-interacao");
+
+    const likeButton = document.createElement("button");
+    likeButton.id = "btn-interacao_like";
+    likeButton.classList.add("botao_interacao");
+
+    const likeImage = document.createElement("img");
+    likeImage.src = "../img/icons/coracao.svg";
+    likeImage.classList.add("icon-interacao");
+
+    likeButton.appendChild(likeImage);
+
+    const commentButton = document.createElement("button");
+    commentButton.id = "btn-interacao_comentario";
+    commentButton.classList.add("botao_interacao");
+
+    const commentImage = document.createElement("img");
+    commentImage.src = "../img/icons/comentar.svg";
+    commentImage.classList.add("icon-interacao");
+
+    commentButton.appendChild(commentImage);
+
+    divInteractions.appendChild(likeButton);
+    divInteractions.appendChild(commentButton);
+
     div.appendChild(h2);
     div.appendChild(pData);
     div.appendChild(pText);
+    div.appendChild(divInteractions);
 
     containerPosts.appendChild(div);
   });
