56706074704c344cd72e84afb685d33d09f27430
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-06-12 01:23:31 -0300

    feat: events calendar

--- scripts/calendario.js
+++ scripts/calendario.js
@@ -1,116 +1,5 @@
-const calendar = document.getElementById("calendar");
-const monthDisplay = document.getElementById("monthDisplay");
-const backButton = document.getElementById("backButton");
-const nextButton = document.getElementById("nextButton");
-
-const modal = document.getElementById("modal");
-const closeButton = document.querySelector(".close-button");
-const eventDate = document.getElementById("eventDate");
-const eventsContainer = document.getElementById("events");
-const eventoMostrado = document.getElementsByClassName(
-  "mostrar-eventos-container"
-);
-
-let currentDate = new Date();
-let events = {}; // Object to store events
-
-backButton.addEventListener("click", () => {
-  currentDate.setMonth(currentDate.getMonth() - 1);
-  renderCalendar();
-});
-
-nextButton.addEventListener("click", () => {
-  currentDate.setMonth(currentDate.getMonth() + 1);
-  renderCalendar();
-});
-
-function renderCalendar() {
-  calendar.innerHTML = "";
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth();
-
-  const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
-  const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-
-  const firstDayIndex = firstDayOfMonth.getDay();
-  const lastDayIndex = lastDayOfMonth.getDay();
-  const lastDayPrevMonth = new Date(currentYear, currentMonth, 0).getDate();
-
-  const daysInMonth = lastDayOfMonth.getDate();
-
-  monthDisplay.innerText = currentDate.toLocaleDateString("pt-BR", {
-    month: "long",
-    year: "numeric",
-  });
-
-  // Display days from previous month
-  for (let i = firstDayIndex; i > 0; i--) {
-    const day = document.createElement("div");
-    day.classList.add("day", "other-month");
-    day.innerText = lastDayPrevMonth - i + 1;
-    calendar.appendChild(day);
-  }
-
-  // Display days of current month
-  for (let i = 1; i <= daysInMonth; i++) {
-    const day = document.createElement("div");
-    day.classList.add("day");
-    day.innerText = i;
-    day.addEventListener("click", () => openModal(i));
-    calendar.appendChild(day);
-  }
-
-  // Display days from next month
-  for (let i = 1; i < 7 - lastDayIndex; i++) {
-    const day = document.createElement("div");
-    day.classList.add("day", "other-month");
-    day.innerText = i;
-    calendar.appendChild(day);
-  }
-}
-
-function openModal(day) {
-  const selectedDate = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth(),
-    day
-  );
-  eventDate.innerText = selectedDate.toLocaleDateString("pt-BR", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  eventsContainer.innerHTML = "";
-  const dayEvents = events[selectedDate.toDateString()] || [];
-  if (dayEvents.length > 0) {
-    dayEvents.forEach((event) => {
-      const eventElement = document.createElement("div");
-      eventElement.innerText = event;
-      eventsContainer.appendChild(eventElement);
-    });
-  } else {
-    eventsContainer.innerText = "Nenhum evento";
-  }
-
-  modal.style.display = "block";
-}
-
-function closeModal() {
-  modal.style.display = "none";
-}
-
-closeButton.addEventListener("click", closeModal);
-window.addEventListener("click", (event) => {
-  if (event.target == modal) {
-    closeModal();
-  }
-});
-
-renderCalendar();
-
 const getEvents = async () => {
-  let events;
+  let events = [];
   await axios
     .get("http://localhost:8081/events", {
       headers: {
@@ -121,6 +10,172 @@ const getEvents = async () => {
       events = res.data;
     })
     .catch((err) => console.log(err));
+
+  const calendar = document.getElementById("calendar");
+  const monthDisplay = document.getElementById("monthDisplay");
+  const backButton = document.getElementById("backButton");
+  const nextButton = document.getElementById("nextButton");
+
+  const modal = document.getElementById("modal");
+  const closeButton = document.querySelector(".close-button");
+  const eventDate = document.getElementById("eventDate");
+  const eventsContainer = document.getElementById("events");
+  const eventoMostrado = document.getElementsByClassName(
+    "mostrar-eventos-container"
+  );
+
+  let currentDate = new Date();
+
+  backButton.addEventListener("click", () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    renderCalendar();
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    renderCalendar();
+  });
+
+  function renderCalendar() {
+    calendar.innerHTML = "";
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+    const firstDayIndex = firstDayOfMonth.getDay();
+    const lastDayIndex = lastDayOfMonth.getDay();
+    const lastDayPrevMonth = new Date(currentYear, currentMonth, 0).getDate();
+
+    const daysInMonth = lastDayOfMonth.getDate();
+
+    monthDisplay.innerText = currentDate.toLocaleDateString("pt-BR", {
+      month: "long",
+      year: "numeric",
+    });
+
+    // Display days from previous month
+    for (let i = firstDayIndex; i > 0; i--) {
+      const day = document.createElement("div");
+      day.classList.add("day", "other-month");
+      day.innerText = lastDayPrevMonth - i + 1;
+      calendar.appendChild(day);
+    }
+
+    // Display days of current month
+    for (let i = 1; i <= daysInMonth; i++) {
+      const day = document.createElement("div");
+      day.classList.add("day");
+
+      // Verificar se há eventos no dia atual
+      const eventDay = events.some((event) => {
+        const eventStartDate = new Date(event.startDate);
+        return (
+          eventStartDate.getDate() === i &&
+          eventStartDate.getMonth() === currentMonth &&
+          eventStartDate.getFullYear() === currentYear
+        );
+      });
+
+      if (eventDay) {
+        day.classList.add("event-day");
+      }
+
+      day.innerText = i;
+      day.addEventListener("click", () => openModal(i));
+      calendar.appendChild(day);
+    }
+
+    // Display days from next month
+    for (let i = 1; i < 7 - lastDayIndex; i++) {
+      const day = document.createElement("div");
+      day.classList.add("day", "other-month");
+      day.innerText = i;
+      calendar.appendChild(day);
+    }
+  }
+
+  function openModal(day) {
+    const selectedDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      day
+    );
+    eventDate.innerText = selectedDate.toLocaleDateString("pt-BR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+
+    eventsContainer.innerHTML = "";
+    const dayEvents = events.filter((event) => {
+      const eventStartDate = new Date(event.startDate);
+      // Verifica se o evento ocorre no mesmo dia que o selectedDate
+      return (
+        eventStartDate.getDate() === selectedDate.getDate() &&
+        eventStartDate.getMonth() === selectedDate.getMonth() &&
+        eventStartDate.getFullYear() === selectedDate.getFullYear()
+      );
+    });
+
+    if (dayEvents.length > 0) {
+      dayEvents.forEach((event) => {
+        const eventElement = document.createElement("div");
+
+        const h2 = document.createElement("h2");
+        h2.innerText = event.title;
+        eventElement.appendChild(h2);
+
+        const inicio = document.createElement("p");
+        inicio.innerText = `Início: ${formatDateTime(event.startDate)}`;
+        eventElement.appendChild(inicio);
+
+        const fim = document.createElement("p");
+        fim.innerText = `Fim: ${formatDateTime(event.endDate)}`;
+        eventElement.appendChild(fim);
+
+        const local = document.createElement("p");
+        local.innerText = `Local: ${event.location}`;
+        eventElement.appendChild(local);
+
+        const registerButton = document.createElement("button");
+        registerButton.classList.add("btn-participar-modal");
+        registerButton.innerText = "Participar";
+
+        const buttonImage = document.createElement("img");
+        buttonImage.src = "../img/icons/participar-icon.svg";
+        registerButton.appendChild(buttonImage);
+        eventElement.appendChild(registerButton);
+
+        registerButton.addEventListener("click", () => {
+          Toastify({
+            text: "Presença confirmada com sucesso!",
+            duration: 2000,
+          }).showToast();
+        });
+
+        eventsContainer.appendChild(eventElement);
+      });
+    } else {
+      eventsContainer.innerText = "Nenhum evento";
+    }
+
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    modal.style.display = "none";
+  }
+
+  closeButton.addEventListener("click", closeModal);
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      closeModal();
+    }
+  });
+
+  renderCalendar();
 
   const constainerEvents = document.querySelector(".mostrar-eventos-container");
   events.forEach((event) => {
@@ -166,3 +221,18 @@ const getEvents = async () => {
 };
 
 getEvents();
+
+const formatDateTime = (dateTimeString) => {
+  const date = new Date(dateTimeString);
+
+  const day = date.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `dia ${day} às ${hours}h${minutes}`;
+};
